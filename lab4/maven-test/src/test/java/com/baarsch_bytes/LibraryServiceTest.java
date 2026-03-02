570c8d5185e83bd223ef44617a30a46f21b07cfe
harithh0 <169714068+harithh0@users.noreply.github.com>
2026-03-02 15:08:33 -0600

    added beforeEach

--- lab4/maven-test/src/test/java/com/baarsch_bytes/LibraryServiceTest.java
+++ lab4/maven-test/src/test/java/com/baarsch_bytes/LibraryServiceTest.java
@@ -5,6 +5,7 @@ import com.baarsch_bytes.Exceptions.EmailFailureException;
 import com.baarsch_bytes.Library.EmailProvider;
 import com.baarsch_bytes.Library.LibraryService;
 import com.baarsch_bytes.Library.ResourceRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -27,9 +28,15 @@ public class LibraryServiceTest {
     @Mock
     ResourceRepository resourceRepositoryMock;
 
+    LibraryService service;
+
+    @BeforeEach
+    public void setUp() {
+        service = new LibraryService(emailProviderMock, resourceRepositoryMock);
+    }
+
     @Test
     public void checkoutResource_nullUUID_returnsFalse() throws EmailFailureException, DatabaseFailureException {
-        LibraryService service = new LibraryService(emailProviderMock, resourceRepositoryMock);
         assertFalse(service.checkoutResource(null, "test@gmail.com"));
     }
 
@@ -39,7 +46,6 @@ public class LibraryServiceTest {
         when(resourceRepositoryMock.isResourceAvailable(uuid)).thenReturn(true);
         when(resourceRepositoryMock.updateStatus(uuid, false)).thenReturn(true);
         when(emailProviderMock.sendEmail("test@gmail.com", "Resource ID: " + uuid + " checked out.")).thenReturn(true);
-        LibraryService service = new LibraryService(emailProviderMock, resourceRepositoryMock);
         assertTrue(service.checkoutResource(uuid, "test@gmail.com"));
     }
 
@@ -47,7 +53,6 @@ public class LibraryServiceTest {
     public void resourceAvailableFalse_throwsFalse() throws Exception  {
         UUID uuid = UUID.randomUUID();
         when(resourceRepositoryMock.isResourceAvailable(uuid)).thenReturn(false);
-        LibraryService service = new LibraryService(emailProviderMock, resourceRepositoryMock);
         assertFalse(service.checkoutResource(uuid, "test@gmail.com"));
     }
 
@@ -58,7 +63,6 @@ public class LibraryServiceTest {
         when(resourceRepositoryMock.isResourceAvailable(uuid)).thenReturn(true);
         when(resourceRepositoryMock.updateStatus(uuid, false)).thenReturn(true);
         when(emailProviderMock.sendEmail("test@gmail.com", "Resource ID: " + uuid + " checked out.")).thenReturn(false);
-        LibraryService service = new LibraryService(emailProviderMock, resourceRepositoryMock);
         assertThrows(EmailFailureException.class, () -> { service.checkoutResource(uuid, "test@gmail.com");});
     }
 
@@ -73,3 +77,4 @@ public class LibraryServiceTest {
 
 
 }
+
